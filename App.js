d5eab86707081aeb7d42cdd7901768e3c98f39ca
Kris-fynn <prokris.fynn@gmail.com>
2022-08-08 11:58:32 +0200

    init commit

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { GOOGLE_API_KEY } from "react-native-dotenv";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
---
+++ components/SearchBar/SearchBar.js
@@ -0,0 +1,8 @@
+import React, {Component} from "react";
+import { SearchBar } from "react-native-elements";
+exort default class Search extends component { 
+  render() {
+    
+  }
+
+}
